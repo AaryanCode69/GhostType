edc3222757435160a01814b3fe84aacbf0d002bd
AaryanCode69 <aaryanupadhyay68@gmail.com>
2026-05-29 01:19:13 +0530

    core: schema design for user

---
+++ app/models/user.py
@@ -0,0 +1,21 @@
+from  core.database import Base
+from sqlalchemy import Column,DateTime,String,UUID,Boolean
+from sqlalchemy.sql import func
+
+class BaseClass:
+    id = Column(UUID,primary_key= True,index= True)
+    created_at = Column(DateTime(timezone=True),server_default=func.now())
+    updated_at = Column(DateTime(timezone=True),server_default=func.now(),onupdate=func.now())
+
+
+class User(Base,BaseClass):
+    __tablename__ = "users"
+    
+    email = Column(String,index=True,nullable=False,unique=True)
+    hashed_password = Column(String,nullable=False)
+    username = Column(String,index= True,nullable= False,unique=True)
+    avatar_url = Column(String,nullable= True)
+    is_active = Column(Boolean,default= True)
+    is_superuser = Column(Boolean,default=False)
+
+
